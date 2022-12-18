008130f722ed11ad0c8169cfe1bc43d7900676da
Christoph Grabo <asaaki@mannaz.cc>
2022-12-18 11:25:28 +0100

    Clean up tracing setup

--- cti_core/src/tracing.rs
+++ cti_core/src/tracing.rs
@@ -3,9 +3,7 @@ use crate::types::AppResult;
 use tracing_subscriber::{filter::EnvFilter, layer::SubscriberExt, util::SubscriberInitExt};
 
 pub(crate) fn start_tracing() -> AppResult {
-    // let fmt_layer = tracing_subscriber::fmt::layer().compact();
-    // let fmt_layer = tracing_subscriber::fmt::layer().without_time();
-    let fmt_layer = tracing_subscriber::fmt::layer();
+    let fmt_layer = tracing_subscriber::fmt::layer().without_time();
 
     tracing_subscriber::registry()
         .with(EnvFilter::from_default_env())
